a01149d210b0202226f9087c25eb64bf9663305d
oviedoem <ferreteriaoviedo.elmanzano@gmail.com>
2026-07-03 19:48:27 -0400

    V37 2026-07-03 19:48 -- actualizacion automatica

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
 // BUILD_DATE se actualiza automáticamente al hacer deploy
 // ============================================================
 
-const BUILD_DATE = '2026-07-03 15:06:54'; // <- actualizado por update-sw-version.js // ← actualizado por update-sw-version.js // ← actualizado por update-sw-version.js
+const BUILD_DATE = '2026-07-03 19:45:22'; // <- actualizado por update-sw-version.js // ← actualizado por update-sw-version.js // ← actualizado por update-sw-version.js
 const CACHE_NAME = 'oviedo-' + BUILD_DATE.replace(/[^0-9]/g,'').slice(0,12);
 
 // Assets estáticos que se cachean en instalación (NO incluir HTML)
